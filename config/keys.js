bdefdab458cb2e9c69b5657aaed1840e182fe651
poornachander <chander.raparti@gmail.com>
2021-08-28 20:10:01 +0530

    port
    changed port

--- config/keys.js
+++ config/keys.js
@@ -2,9 +2,12 @@ module.exports = {
     clientID: "160174807802-rn22k2niqljh75hi308v7ctji379cbef.apps.googleusercontent.com",
     clientSecret: "bBBbBew7mdGeN7Oyd0efFD9p",
     mongoURI: "mongodb+srv://PR-Test:poorna9666@test.cpqoh.mongodb.net/education?retryWrites=true&w=majority",
-    sessionSecret: "abc2"
+    sessionSecret: "abc2",
+    PORT:"https://dashboard.heroku.com"
   };
   
 // Super User Details :- 
 // Email Id :- eduon.portal@gmail.com
 // Password :- lav1234@ 
+
+
