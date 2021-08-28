754469bd51f8cdc040a5c64c8ffd9754b14249c7
poornachander <chander.raparti@gmail.com>
2021-08-28 19:51:02 +0530

    Initial commit

---
+++ config/keys.js
@@ -0,0 +1,10 @@
+module.exports = {
+    clientID: "160174807802-rn22k2niqljh75hi308v7ctji379cbef.apps.googleusercontent.com",
+    clientSecret: "bBBbBew7mdGeN7Oyd0efFD9p",
+    mongoURI: "mongodb+srv://PR-Test:poorna9666@test.cpqoh.mongodb.net/education?retryWrites=true&w=majority",
+    sessionSecret: "abc2"
+  };
+  
+// Super User Details :- 
+// Email Id :- eduon.portal@gmail.com
+// Password :- lav1234@ 
